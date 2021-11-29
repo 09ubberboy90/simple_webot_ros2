252da18f5938974cb2604e0f14b329f05bec241a
09ubberboy90 <2330834a@student.gla.ac.uk>
2021-11-29 20:05:15 +0000

    Added back missing proto from simple_arm

--- webots_driver/setup.py
+++ webots_driver/setup.py
@@ -15,6 +15,8 @@ setup(
         ('share/' + package_name + '/config', glob('config/*.*')),
         ('share/' + package_name + '/urdf', glob('urdf/*.*')),
         ('share/' + package_name + '/worlds', glob('worlds/*.*')),
+        ('share/' + package_name +'/protos', ['protos/panda.proto'])
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
