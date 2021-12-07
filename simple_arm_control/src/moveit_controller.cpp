a7d797db5207057eed90605435fb83a194f57366
09ubberboy90 <2330834a@student.gla.ac.uk>
2021-12-07 18:40:42 +0000

    Streamline changes from other simulator

--- simple_arm_control/src/moveit_controller.cpp
+++ simple_arm_control/src/moveit_controller.cpp
@@ -106,7 +106,7 @@ int main(int argc, char **argv)
 
         pose.position.x = 0.6;
         pose.position.y = 0.0;
-        pose.position.z = (0.37) + i*0.05; 
+        pose.position.z = (0.4) + i*0.05; 
         
 
         success = simple_moveit->place(obj_name, pose);
@@ -129,7 +129,7 @@ int main(int argc, char **argv)
         }
     }
     RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Going to start pose");
-    simple_moveit->goto_pose(simple_moveit->get_move_group(), start_pose);
+    simple_moveit->goto_pose(start_pose);
     rclcpp::shutdown();
     return 0;
 }
