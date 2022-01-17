4508a81555e56b79cc9696ac76e5d80848c820fd
09ubberboy90 <2330834a@student.gla.ac.uk>
2022-01-17 16:34:27 +0000

    Clean up recorder

--- sim_recorder/sim_recorder/proc_monitor.py
+++ sim_recorder/sim_recorder/proc_monitor.py
@@ -35,15 +35,15 @@ from collections import defaultdict
 
 import psutil
 import rclpy
-from rclpy.node import Node
 
 
-class ProcMonitor(Node):
+
+class ProcMonitor():
     """
     Create the main window and connect the menu bar slots.
     """
 
-    def __init__(self, allowed, idx, sim_name):
+    def __init__(self, allowed, idx, sim_name, path):
         super().__init__('proccess_monitor')
         self.cpu_dict = defaultdict(list)
         self.ram_dict = defaultdict(list)
@@ -56,6 +56,7 @@ class ProcMonitor(Node):
         self.timer = self.create_timer(0.1, self.animate)
         self.idx = idx
         self.sim_name = sim_name
+        self.path = path
 
     def update_missing(self):
         length = len(self.procs.keys())
@@ -68,7 +69,7 @@ class ProcMonitor(Node):
 
             for proc in psutil.process_iter():
                 p = proc.name()
-                if p in self.allowed and proc.pid not in self.procs.keys():
+                if proc.pid not in self.procs.keys():
                     self.procs[proc.pid] = proc
                     self.pids[proc.pid] = proc.name()
                     proc.cpu_percent()  # discard value
@@ -101,11 +102,10 @@ class ProcMonitor(Node):
                 name = name+"_"+str(counter)
             self.name[pid] = name
 
-        path = "/home/ubb/Documents/PersonalProject/VrController/sim_recorder/data/"
-        with open(path + f"{self.sim_name}/cpu/cpu_{self.idx}.csv", "w") as f:
+        with open(self.path + f"{self.sim_name}/cpu/cpu_{self.idx}.csv", "w") as f:
             for pid, el in self.cpu_dict.items():
                 f.write(f"{self.name[pid]},{','.join(str(v) for v in el)}\n")
-        with open(path + f"{self.sim_name}/ram/ram_{self.idx}.csv", "w") as f:
+        with open(self.path + f"{self.sim_name}/ram/ram_{self.idx}.csv", "w") as f:
             for pid, el in self.ram_dict.items():
                 f.write(f"{self.name[pid]},{','.join(str(v) for v in el)}\n")
         sys.exit(0)
@@ -167,9 +167,8 @@ allowed_vr = [
 ]
 
 
-def run(simulator="webots", idx=0, args=None):
+def run(path, simulator="webots", idx=0):
 
-    rclpy.init(args=args)
     if "webots" == simulator:
         allowed = allowed_webots[1:]
     elif "gazebo" == simulator:
@@ -183,17 +182,7 @@ def run(simulator="webots", idx=0, args=None):
     else:
         allowed = allowed_ignition
 
-    monitor = ProcMonitor(allowed, idx, simulator)
+    monitor = ProcMonitor(allowed, idx, simulator, path)
     signal.signal(signal.SIGINT, lambda sig, frame: monitor.dump_values())
     signal.signal(signal.SIGTERM, lambda sig, frame: monitor.dump_values())
 
-    rclpy.spin(monitor)
-    rclpy.shutdown()
-
-
-def main(args=None):
-    run(args=args)
-
-
-if __name__ == '__main__':
-    main()
--- sim_recorder/sim_recorder/run_recording.py
+++ sim_recorder/sim_recorder/run_recording.py
@@ -40,13 +40,24 @@ import psutil
 
 try:
     import proc_monitor
-    import proc_monitor_gui
 except ModuleNotFoundError:
     from . import proc_monitor
-    from . import proc_monitor_gui
 
 import _thread
 import threading
+
+class Webots():
+    def __init__(self):
+        self.name = "webots"
+        self.timeout = 300 # 5 minute
+        self.commands = [
+            "ros2 launch webots_driver panda.launch.py",
+            "ros2 launch webots_driver run_move_group.launch.py",
+            "ros2 launch webots_driver collision.launch.py",
+            "ros2 launch webots_driver moveit_controller.launch.py",
+        ]
+        self.delays = [5, 10, 5]
+
 
 
 def kill_proc_tree(pids, procs, interrupt_event, including_parent=False):
@@ -60,10 +71,10 @@ def kill_proc_tree(pids, procs, interrupt_event, including_parent=False):
                 parent.kill()
         except:
             pass
-    time.sleep(2)  # Wait for everything ot close to prevent broken_pipe
+    time.sleep(5)  # Wait for everything ot close to prevent broken_pipe
     for proc in procs[:-1]:
         proc.kill()
-    time.sleep(2)  # Wait for everything ot close to prevent broken_pipe
+    time.sleep(5)  # Wait for everything ot close to prevent broken_pipe
 
 
 # Reference : https://stackoverflow.com/a/40281422
@@ -78,32 +89,30 @@ def run_com(w, q, com):
     q.put(proc.pid)
 
 
-def run_recorder(q, interrupt_event, simulator, idx, gui=False):
+def run_recorder(q, interrupt_event, simulator, idx, path):
     task = threading.Thread(target=interrupt_handler, args=(interrupt_event,))
     task.start()
     try:
-        if gui:
-            proc_monitor_gui.run(simulator, idx=idx)  # launch recording
-        else:
-            proc_monitor.run(simulator, idx=idx)
+        proc_monitor.run(simulator=simulator, idx=idx, path=path)
     except:
         q.put("Exit")
 
 
-def generate_procs(simulator, commands, r, w, q, interrupt_event, idx):
+def generate_procs(simulator, commands, r, w, q, interrupt_event, idx, path):
     procs = []
     for com in commands:
         procs.append(Process(target=run_com, args=(w, q, com), name=com))
     procs.append(Process(target=run_recorder, args=(
-        q, interrupt_event, simulator, idx), daemon=True, name="Recorder"))
+        q, interrupt_event, simulator, idx, path), daemon=True, name="Recorder"))
     return procs
 
 
 def start_proces(delay, procs, q):
     pids = []
-    delay.append(0)  # Out of bound exception prevention
-    delay.append(0)  # Out of bound exception prevention
+    for _ in range(len(procs) - len(delay)):
+        delay.append(0)
     for idx, p in enumerate(procs):
+        print(p.name)
         p.start()
         time.sleep(delay[idx])
 
@@ -112,152 +121,43 @@ def start_proces(delay, procs, q):
 
     return pids
 
-
-class Webots():
-    def __init__(self):
-        self.name = "webots"
-        self.timeout = 30
-        self.commands = [
-            "ros2 launch webots_simple_arm pick_place.launch.py",
-            "ros2 launch webots_simple_arm collision_webots.launch.py",
-            "ros2 launch webots_simple_arm moveit_webots.launch.py",
-        ]
-        self.delays = [5, 5]
-
-
-class Gazebo():
-    def __init__(self):
-        self.name = "gazebo"
-        self.timeout = 30
-        self.commands = [
-            "ros2 launch simple_arm gazebo.launch.py",
-            "ros2 launch simple_move_group run_move_group.launch.py",
-            "ros2 launch simple_arm collision_gazebo.launch.py",
-            "ros2 launch simple_arm moveit_gazebo.launch.py",
-        ]
-        self.delays = [5, 5, 5]
-
-
-class Ignition():
-    def __init__(self):
-        self.name = "ignition"
-        self.timeout = 35
-        self.commands = [
-            "ros2 launch simple_arm ign_place.launch.py",
-            "ros2 launch simple_move_group run_move_group.launch.py",
-            "ros2 launch simple_arm moveit_ign.launch.py",
-        ]
-        self.delays = [1, 5]
-
-
-class GithubIgnition():
-    def __init__(self):
-        self.name = "github_ignition"
-        self.timeout = 25
-        self.commands = [
-            "ros2 launch ign_moveit2 example_place.launch.py",
-        ]
-        self.delays = []
-
-
-class WebotsThrow():
-    def __init__(self):
-        self.name = "webots_throw"
-        self.timeout = 40
-        self.commands = [
-            "ros2 launch webots_simple_arm pick_place.launch.py",
-            "ros2 launch webots_simple_arm throw_collision.launch.py",
-            "ros2 launch webots_simple_arm throw_moveit.launch.py",
-        ]
-        self.delays = [5, 5]
-
-
-class GazeboThrow():
-    def __init__(self):
-        self.name = "gazebo_throw"
-        self.timeout = 40
-        self.commands = [
-            "ros2 launch simple_arm gazebo.launch.py",
-            "ros2 launch simple_move_group run_move_group.launch.py",
-            "ros2 launch simple_arm throw_collision.launch.py",
-            "ros2 launch simple_arm throw_moveit.launch.py",
-        ]
-        self.delays = [5, 5, 5]
-
-
-class IgnitionThrow():
-    def __init__(self):
-        self.name = "ignition_throw"
-        self.timeout = 30
-        self.commands = [
-            "ros2 launch simple_arm ign_throw.launch.py",
-            "ros2 launch simple_move_group run_move_group.launch.py",
-            "ros2 launch simple_arm ign_throw_moveit.launch.py",
-        ]
-        self.delays = [1, 5]
-
-
-class GithubIgnitionThrow():
-    def __init__(self):
-        self.name = "github_ignition_throw"
-        self.timeout = 25
-        self.commands = [
-            "ros2 launch ign_moveit2 example_throw.launch.py",
-        ]
-        self.delays = []
-
-
-class VR():
-    def __init__(self):
-        self.name = "vr"
-        self.timeout = 999
-        self.commands = [
-            "ros2 launch simple_arm ign_vr.launch.py",
-        ]
-        self.delays = []
-
-
 def handler(signum, frame):
-    raise Exception("TimeOut")
+    raise Exception("Timeout")
 
-
-def run(sim, idx):
-    path = "/home/ubb/Documents/PersonalProject/VrController/sim_recorder/data/"
+def log(file, msg):
+    print(msg)
+    file.write(msg + "\n")
+    
+def run(sim, idx, path):
     r, w = Pipe()
     q = Queue()
     reader = os.fdopen(r.fileno(), 'r')
     interrupt_event = Event()
     procs = generate_procs(sim.name, sim.commands, r,
-                           w, q, interrupt_event, idx)
+                           w, q, interrupt_event, idx, path)
     time.sleep(1)
     pids = start_proces(sim.delays, procs, q)
     signal.signal(signal.SIGALRM, handler)
     signal.alarm(sim.timeout)
-    start_time = time.time()
-    with open(path+f"{sim.name}/log/{idx}.txt", "w") as f,\
-            open(path+f"{sim.name}/run.txt", "a") as out:
+    with open(path+f"/{sim.name}/log/{idx}.txt", "w") as f,\
+            open(path+f"/{sim.name}/run.txt", "a") as out:
         try:
             while True:
                 text = reader.readline()
                 f.write(text)
-                if "Task completed Succesfully" in text or "ODE INTERNAL ERROR" in text:
-                    print(
-                        f"Completed for {idx} in {time.time() - start_time }")
-                    out.write(
-                        f"Completed for {idx} in {time.time() - start_time }\n")
+                timing = 0
+                if "Task executed successfully" in text:
+                    log(out, f"Completed for {idx} in {timing}")
                     signal.alarm(0)
                     kill_proc_tree(pids, procs, interrupt_event)
                     return 1, 0
-                if "Cube is not in bound" in text:
-                    print(f"Failed for {idx} in {time.time() - start_time }")
-                    out.write(
-                        f"Failed for {idx} in {time.time() - start_time }\n")
+                if "Cube is not in bound" in text: 
+                    log(out, f"Failed for {idx} in {timing}")
                     signal.alarm(0)
                     kill_proc_tree(pids, procs, interrupt_event)
                     return 0, 1
         except:
-            print(f"Timeout for {idx}")
-            out.write(f"Timeout for {idx}\n")
+            log(out,f"Timeout for {idx}")
             f.write("Timeout")
             kill_proc_tree(pids, procs, interrupt_event)
             return 0, 0
@@ -266,37 +166,20 @@ def run(sim, idx):
 def main(args=None):
     succ = 0
     fail = 0
-    if len(sys.argv) == 1:
-        raise Exception("Missing arguments")
-    if sys.argv[1] == "webots":
-        sim = Webots()
-    elif sys.argv[1] == "webots_throw":
-        sim = WebotsThrow()
-    elif sys.argv[1] == "gazebo":
-        sim = Gazebo()
-    elif sys.argv[1] == "gazebo_throw":
-        sim = GazeboThrow()
-    elif sys.argv[1] == "github_ignition":
-        sim = GithubIgnition()
-    elif sys.argv[1] == "github_ignition_throw":
-        sim = GithubIgnitionThrow()
-    elif sys.argv[1] == "ignition":
-        sim = Ignition()
-    elif sys.argv[1] == "ignition_throw":
-        sim = IgnitionThrow()
-    elif sys.argv[1] == "vr":
-        sim = VR()
 
-    if len(sys.argv) == 3:
-        iteration = int(sys.argv[2])
+    sim = Webots()
+
+    if len(sys.argv) == 2:
+        iteration = int(sys.argv[1])
     else:
         iteration = 1
-    path = "/home/ubb/Documents/PersonalProject/VrController/sim_recorder/data/"
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(dir_path, "..", "data")
     try:
-        os.mkdir(path+f"{sim.name}")
-        os.mkdir(path+f"{sim.name}/log")
-        os.mkdir(path+f"{sim.name}/ram")
-        os.mkdir(path+f"{sim.name}/cpu")
+        os.mkdir(path+f"/{sim.name}")
+        os.mkdir(path+f"/{sim.name}/log")
+        os.mkdir(path+f"/{sim.name}/ram")
+        os.mkdir(path+f"/{sim.name}/cpu")
     except Exception as e:
         print(e)
         print("Folder exist. Overwriting...")
@@ -304,7 +187,7 @@ def main(args=None):
         os.remove(path+f"{sim.name}/run.txt")
 
     for idx in range(iteration):
-        a, b = run(sim, idx)
+        a, b = run(sim, idx, path)
         succ += a
         fail += b
     print(f"Success {succ}; Failure {fail}; Timeout {iteration-(succ + fail)}")
